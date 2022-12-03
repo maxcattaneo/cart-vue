9885c16265ba7feb37c69209627803136a8197b3
Maximiliano Cattáneo <maximilianodamiancattaneo@gmail.com>
2022-12-03 16:35:30 +0100

    "nav bar"

--- cart-app/src/App.vue
+++ cart-app/src/App.vue
@@ -1,14 +1,15 @@
 <template>
     <body>
-        <header class="bg-gray-700 flex justify-center text-white h-16">
-            <nav class="flex items-center">
-                <ul>Allproduct</ul>
-                <ul>SmartPhones</ul>
-                <ul>Notebooks</ul>
-            </nav>
-            <button class="bg-sky-500/100 p-2">cart</button>
+        <header class="h-20 bg-gray-700 flex justify-center items-center">
+            <div class="bg-gray-700 flex justify-center text-white h-10 w-full ">
+                <nav class="flex items-center justify-evenly w-1/2">
+                    <ul>Allproduct</ul>
+                    <ul>SmartPhones</ul>
+                    <ul>Notebooks</ul>
+                </nav>
+                <button class="bg-sky-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">Cart</button>
+            </div>
         </header>
-        
     </body>
 
   
