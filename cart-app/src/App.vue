bfa1715fb8ed40d56919bc2afe5d5d3b7a5aeb5f
Maximiliano Cattáneo <maximilianodamiancattaneo@gmail.com>
2022-12-03 15:33:15 +0100

    "cart con vue y tailwind"

---
+++ cart-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <body>
+        <header class="bg-gray-700 flex justify-center text-white h-16">
+            <nav class="flex items-center">
+                <ul>Allproduct</ul>
+                <ul>SmartPhones</ul>
+                <ul>Notebooks</ul>
+            </nav>
+            <button class="bg-sky-500/100 p-2">cart</button>
+        </header>
+        
+    </body>
+
+  
+</template>
+
+<script setup>
+import {ref} from "vue";
+</script>
+
+<style>
+
+</style>
---
+++ first-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Hola {{name}}!</h2>
+  <input type="mail" v-model="email"/>
+  <p :="email">{{email}}</p>
+  <button @click="showEmail">Show email</button>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const name = ref ("Max");
+const email = ref ("maxi@gmail.com");
+function showEmail(){
+  
+}
+
+</script>
+
+<style scoped>
+h2{
+  color:red;
+}
+.show{
+  display: block;
+}
+.hidden{
+  display: none;
+}
+
+</style>
