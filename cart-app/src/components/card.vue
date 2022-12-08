674d55e4b1a9bd0b001dce15b60f885160a9f984
Maximiliano Cattáneo <maximilianodamiancattaneo@gmail.com>
2022-12-08 23:13:54 +0100

    componente card y boton ok

---
+++ cart-app/src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="card" class="w-[300px] h-[420px] m-10 flex flex-col justify-between bg-white p-2 rounded ">
+        <img :src="beer.image_url" class="h-[250px] w-[100px] mx-auto" alt="">
+        <a class="text-xl" href="#">{{beer.name}}</a>
+        <div class="flex justify-between">
+            <span>{{beer.ibu}}€</span>
+            <span>10 X 226,00</span>
+        </div>
+        <Button class="bg-green-500" :text="'Add to cart'"/>
+     </div>
+</template>
+
+<script setup>
+import Button from "./button.vue";
+const props = defineProps(['beer']);
+</script>
+
+<style>
+
+</style>
---
+++ cart-app/src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+        <header class="h-20 bg-gray-700 flex justify-center items-center">
+            <div class="bg-gray-700 flex justify-center text-white h-10 w-full ">
+                <nav class="flex items-center justify-evenly w-1/2">
+                    <a href="#">AllProducts</a>
+                    <a href="#">Smartphones</a>
+                    <a href="#">Notebooks</a>
+                </nav>
+                <Button :text="'Cart'"/>
+            </div>
+        </header>
+  
+</template>
+
+<script setup>
+import Button from "./button.vue"
+
+</script>
+
+<style>
+
+</style>
